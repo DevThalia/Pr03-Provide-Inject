378fb10b8e82d8900354b3fceb1349eb77bd4b0a
Thalia <thalia.soler.7e7@itb.cat>
2024-11-07 17:44:58 +0100

    Título printado correctamente

---
+++ pr03/src/components/order.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from 'vue'
+let titulo = ref('')
+
+</script>
+
+<template>
+    <h1> {{ titulo }}</h1>
+    <input type="text" name="titulo" id="titulo" v-model="titulo">
+    <button type="submit">Place order</button>
+</template>
